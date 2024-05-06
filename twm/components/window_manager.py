c707b5520fe0d8502330dfaaea566d28a1f65b3a
Legendyboi <legendyboi.dev@proton.me>
2024-05-06 22:35:25 +0530

    Not yet.

--- twm/components/window_manager.py
+++ twm/components/window_manager.py
@@ -2,13 +2,15 @@ from Xlib import X, display, XK, Xatom
 import ewmh
 
 class WindowManager():
-    def __init__(self):
+    def __init__(self, prefs):
         #X Server Display and Screen init
         self.display = display.Display()
         self.screen = self.display.screen()
         self.colourmap = self.screen.default_colormap
         self.emwh = ewmh.EWMH()
         self.root = self.screen.root
+
+        self.prefs = prefs
 
         self.display_dimensions = self.get_display_geometry()
         self.window_resize_options = [
@@ -76,14 +78,62 @@ class WindowManager():
         except:
             print("Failed to get Active Window.")
         return window
+    
+    def getWindow_Attr(self, window):
+        try:
+            return window.get_attributes()
+        except:
+            return None
+
+    def getWindow_shortName(self, window):
+            return '0x{:x} [{}]'.format(window.id, self.getWindow_Class(window))
+
+
+    def getWindow_title(self, window):
+        result = None
+        try:
+            result = window.get_wm_name()
+        except:
+            pass
+        if result is None:
+            return self.session_info.session_name
+        return result
+
+
+    def getWindow_State(self, window):
+        return self.ewmh.getWmState(window, str = True)
+
+    def updateWindow_Count(self):
+        if self.deskbar is not None:
+            self.deskbar.set_window_count(len(self.managed_windows))
+
 
     # Window Controls
 
-    def window_manage(self, window):
-        raise NotImplementedError
+    def manageWindow(self, window):
+        attr = self.getWindow_Attr(window)
+        if attr is None:
+            return
+        if attr.override_redirect:
+            return
+        if self.managed_windows(window):
+            return
+        
+        if self.prefs.dev["debug"] == 1:
+            print("Window Found: %s", self.getWindow_shortName(window))
+        self.managed_windows.append(window)
+        self.exposed_windows.append(window)
+        self.window_order = len(self.managed_windows) - 1
+        self.updateWindow_Count()
+
+        window.map()
+        mask = X.EnterWindowMask | X.LeaveWindowMask
+        
     
-    def window_unmanage(self, window):
-        raise NotImplementedError
+    def unmanageWindow(self, window):
+        if self.managed_windows(window):
+            if self.prefs.dev["debug"] == 1:
+                pass
     
-    def window_destroy(self, window):
+    def destroyWindow(self, window):
         raise NotImplementedError
